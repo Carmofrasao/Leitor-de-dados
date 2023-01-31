5d03c90ab15098f55d12853542d7f0527a3fa0da
Anderson <anderfrasao52@gmail.com>
2023-01-31 10:56:38 -0300

    remove ok, altera nem tanto

--- Inicio.py
+++ Inicio.py
@@ -71,16 +71,109 @@ def Preenche():
     janelaIncerir.mainloop()
     return
 
+def alt():
+    planilha['Planilha1'][int(IdAltera.get())][1] = d1a
+    planilha['Planilha1'][int(IdAltera.get())][2] = d2a
+    planilha['Planilha1'][int(IdAltera.get())][3] = d3a
+    planilha['Planilha1'][int(IdAltera.get())][4] = d4a
+    planilha['Planilha1'][int(IdAltera.get())][5] = d5a
+    # save_data("tabela.ods", planilha)
+
+def altera():
+    janelaAltera = Tk()
+    janelaAltera.title("BioDados")
+
+    dado1Label = Label(janelaAltera,text="Digite o Dado1:", font="Arial")
+    dado1Label.grid(column=0, row=0, padx=10, pady=10)
+
+    global d1a
+    d1a = Entry(janelaAltera)
+    d1a.insert(0, planilha['Planilha1'][int(IdAltera.get())][1])
+    d1a["width"] = 25
+    d1a["font"] = "Arial"
+    d1a.grid(column=1, row=0, padx=10, pady=10)
+
+    dado2Label = Label(janelaAltera,text="Digite o Dado2:", font="Arial")
+    dado2Label.grid(column=0, row=1, padx=10, pady=10)
+
+    global d2a
+    d2a = Entry(janelaAltera)
+    d2a.insert(0, planilha['Planilha1'][int(IdAltera.get())][2])
+    d2a["width"] = 25
+    d2a["font"] = "Arial"
+    d2a.grid(column=1, row=1, padx=10, pady=10)
+
+    dado3Label = Label(janelaAltera,text="Digite o Dado3:", font="Arial")
+    dado3Label.grid(column=0, row=2, padx=10, pady=10)
+
+    global d3a
+    d3a = Entry(janelaAltera)
+    d3a.insert(0, planilha['Planilha1'][int(IdAltera.get())][3])
+    d3a["width"] = 25
+    d3a["font"] = "Arial"
+    d3a.grid(column=1, row=2, padx=10, pady=10)
+
+    dado4Label = Label(janelaAltera,text="Digite o Dado4:", font="Arial")
+    dado4Label.grid(column=0, row=3, padx=10, pady=10)
+
+    global d4a
+    d4a = Entry(janelaAltera)
+    d4a.insert(0, planilha['Planilha1'][int(IdAltera.get())][4])
+    d4a["width"] = 25
+    d4a["font"] = "Arial"
+    d4a.grid(column=1, row=3, padx=10, pady=10)
+
+    dado5Label = Label(janelaAltera,text="Digite o Dado5:", font="Arial")
+    dado5Label.grid(column=0, row=4, padx=10, pady=10)
+
+    global d5a
+    d5a = Entry(janelaAltera)
+    d5a.insert(0, planilha['Planilha1'][int(IdAltera.get())][5])
+    d5a["width"] = 25
+    d5a["font"] = "Arial"
+    d5a.grid(column=1, row=4, padx=10, pady=10)
+
+    Inc = Button(janelaAltera, text="Alterar", command=alt)
+    Inc.grid(column=0, row=5, padx=10, pady=10)
+
+    janelaAltera.mainloop()
+    return
+
 def alterar():
     janelaAlterar = Tk()
     janelaAlterar.title("BioDados")
 
+    IdLabel = Label(janelaAlterar,text="Digite o Id:", font="Arial")
+    IdLabel.pack(side=TOP)
+    global IdAltera
+    IdAltera = Entry(janelaAlterar)
+    IdAltera["width"] = 25
+    IdAltera["font"] = "Arial"
+    IdAltera.pack(side=LEFT)
+    
+    Remo = Button(janelaAlterar, text="Alterar", command=altera)
+    Remo.pack(side=BOTTOM)
+
     janelaAlterar.mainloop()
     return
+
+def remov():
+    planilha['Planilha1'].pop(int(IdRemove.get()))
 
 def remover():
     janelaRemover = Tk()
     janelaRemover.title("BioDados")
+
+    IdLabel = Label(janelaRemover,text="Digite o Id:", font="Arial")
+    IdLabel.pack(side=TOP)
+    global IdRemove
+    IdRemove = Entry(janelaRemover)
+    IdRemove["width"] = 25
+    IdRemove["font"] = "Arial"
+    IdRemove.pack(side=LEFT)
+    
+    Remo = Button(janelaRemover, text="Remover", command=remov)
+    Remo.pack(side=BOTTOM)
 
     janelaRemover.mainloop()
     return
@@ -90,7 +183,7 @@ def visualizar():
     janelaVisu.title("BioDados")
     cabecalho = Label(janelaVisu, text=planilha['Planilha1'][0])
     cabecalho.grid(column=0, row=0, padx=10, pady=10)
-    texto = Label(janelaVisu, text=planilha['Planilha1'][int(Id.get())])
+    texto = Label(janelaVisu, text=planilha['Planilha1'][int(IdBusca.get())])
     texto.grid(column=0, row=1, padx=10, pady=10)
     janelaVisu.mainloop()
     return
@@ -101,11 +194,11 @@ def buscar():
 
     IdLabel = Label(janelaBusca,text="Digite o Id:", font="Arial")
     IdLabel.pack(side=TOP)
-    global Id
-    Id = Entry(janelaBusca)
-    Id["width"] = 25
-    Id["font"] = "Arial"
-    Id.pack(side=LEFT)
+    global IdBusca
+    IdBusca = Entry(janelaBusca)
+    IdBusca["width"] = 25
+    IdBusca["font"] = "Arial"
+    IdBusca.pack(side=LEFT)
     
     Visu = Button(janelaBusca, text="visualizar", command=visualizar)
     Visu.pack(side=BOTTOM)
